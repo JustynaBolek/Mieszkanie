4c18cfb24b2bc70fbc26601386976e40ff3a6924
JustynaBolek <justyna.bolek@gmail.com>
2022-09-21 19:48:58 +0200

    added setBalcony method

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         Flat mieszkanie1=new Flat (2,"Wroclaw", true);
         System.out.println(mieszkanie1.getFloor());
         System.out.println(mieszkanie1.getCity());
+        mieszkanie1.setBalcony(false);
         System.out.println(mieszkanie1.getBalcony());
     }
 }
